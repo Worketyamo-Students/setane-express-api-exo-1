477e3a8a234c140dd4b0b7c9b8e3401744bfd0c9
exo <setane122@gmail.com>
2025-05-05 13:33:10 +0100

    feat:get and post

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import chalk from "chalk";
+import { error } from "console";
 
 const app = express();
 
@@ -25,16 +27,20 @@ app.get("/student", (req, res) => {
 });
 
 app.post("/student", (req, res) => {
-  const Id = "worketyamo-" + Math.floor(Math.random() * gen);
-  const db = readDB();
-  req.params.Id = Id;
-  console.log(req.query.Id);
-  // if (req.body.name || req.body.email || req.body.phone) {
-  //   for (let i = 0; i < db.length; i++) {
-  //     if (req.body.id != db[i].id) {
-  //     }
-  //   }
-  // }
-  return res.status(401).json({ msg: { error: "bad request" } });
-  console.log(readDB());
+  if (req.body.name || req.body.email || req.body.phone) {
+    const Id = "worketyamo-" + Math.floor(Math.random() * gen);
+    const student = {
+      id: Id,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+    };
+    const db = readDB();
+    db.push(student);
+    fs.writeFileSync("./database.json", JSON.stringify(db), (err) => {
+      if (err) throw err;
+    });
+    return res.status(200).json({ success: student });
+  }
+  return res.status(401).json({ error: "bad request" });
 });
